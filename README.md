3510cc9e302867d019684ff8bc8a144d96e1f3cc
Cedric May <may@mylane.de>
2015-03-25 18:02:13 +0100

    Initial package configuration

---
+++ README.md
@@ -0,0 +1,1 @@
+# Cloudinary for Meteor
---
+++ package.js
@@ -0,0 +1,13 @@
+Package.describe({
+  name: 'oaf:cloudinary',
+  version: '0.0.1',
+  summary: 'Cloudinary Package for Meteor',
+  git: 'https://github.com/oafmedium/meteor-cloudinary.git',
+  documentation: 'README.md'
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom('1.0.4.2');
+  api.use('coffeescript');
+  api.addFiles('cloudinary.coffee');
+});
