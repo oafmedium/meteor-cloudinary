39cb83eeb7be5dd48740f6712b34eea0a619f6ac
Cedric May <hallo@cedric-may.de>
2015-06-04 14:08:53 +0200

    bump version

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: "oaf:cloudinary",
-  version: "0.0.3",
+  version: "0.0.4",
   summary: "Cloudinary Package for Meteor",
   git: "https://github.com/oafmedium/meteor-cloudinary.git",
   documentation: "README.md"
