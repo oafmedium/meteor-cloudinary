9b0a1f6ea6f75a0f2735439c2e08e69a40cf48b2
Max Nowack <max@unsou.de>
2015-11-03 12:03:41 +0100

    version bump

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: "oaf:cloudinary",
-  version: "0.0.7",
+  version: "0.0.8",
   summary: "Cloudinary Package for Meteor",
   git: "https://github.com/oafmedium/meteor-cloudinary.git",
   documentation: "README.md"
