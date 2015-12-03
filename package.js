bb5cbdc9587ef1d194767dc903c35ee0a8f09e15
Max Nowack <max@unsou.de>
2015-12-03 22:25:13 +0100

    update cloudinary_js

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: "oaf:cloudinary",
-  version: "0.0.8",
+  version: "0.0.9",
   summary: "Cloudinary Package for Meteor",
   git: "https://github.com/oafmedium/meteor-cloudinary.git",
   documentation: "README.md"
@@ -15,7 +15,7 @@ Package.onUse(function(api) {
   api.addFiles("server.coffee", "server");
   api.addFiles([
     "cloudinary_js/js/jquery.fileupload.js",
-    "cloudinary_js/js/load-image.min.js",
+    "cloudinary_js/js/load-image.all.min.js",
     "cloudinary_js/js/canvas-to-blob.min.js",
     "cloudinary_js/js/jquery.fileupload-process.js",
     "cloudinary_js/js/jquery.fileupload-image.js",
